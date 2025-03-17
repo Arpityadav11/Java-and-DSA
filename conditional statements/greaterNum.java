0d8bc9c33c8bbe3722b62ff9fd048eede5abac4f
Arpit yadav <arpityadav112005@gmail.com>
2025-03-17 15:01:19 +0530

    Create greaterNum.java

---
+++ conditional statements/greaterNum.java
@@ -0,0 +1,26 @@
+// print largest of the three numbers 
+import java.util.*;
+
+public class greaterNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the three numbers respectively");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if(a>b){
+            if (a>c) {
+                System.out.println(a+" is greater");
+            }else{
+                System.out.println(c+ " is greater");
+            }
+        }else{
+            if (b>c) {
+                System.out.println(b+" is greater");
+            }else{
+                System.out.println(c+ " is greater");
+            }
+        }
+    }
+}
