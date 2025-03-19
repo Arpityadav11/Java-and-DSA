a4433eac5197d76d74f8b4bbdd0c50ac6c6b6110
Arpit yadav <arpityadav112005@gmail.com>
2025-03-19 22:48:31 +0530

    Create binToDec.java

---
+++ basics/binToDec.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class binToDec {
+
+    public static void binToDec(int binNum){
+        int myNum = binNum;
+        int decNum = 0;
+        int pow = 0;
+        while(binNum>0){
+            int lastDigit = binNum%10;
+            decNum = decNum+(lastDigit*(int)Math.pow(2, pow));
+            pow ++;
+            binNum= binNum/10;
+        }
+        System.out.println("the decimal number of "+myNum+" is: "+decNum);
+    }
+    public static void main(String[] args) {
+        binToDec(101);
+    }
+}
