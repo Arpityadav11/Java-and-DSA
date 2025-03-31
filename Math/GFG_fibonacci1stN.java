a4e6704e85a8f45b7d769264320a0b9d80358e2b
Arpit yadav <arpityadav112005@gmail.com>
2025-03-31 19:13:58 +0530

    Create GFG_fibonacci1stN.java

---
+++ Math/GFG_fibonacci1stN.java
@@ -0,0 +1,18 @@
+class GFG_fibonacci1stN {
+    // Function to return list containing first n fibonacci numbers.
+    public static int[] fibonacciNumbers(int n) {
+        int arr[] = new int[n];
+        int f1 = 0,f2 = 1,next=0;
+        for(int i=0;i<n;i++){
+            if(i<=1){
+            arr[i]=i;
+        }else{
+            next = f1+f2;
+            arr[i]=next;
+            f1=f2;
+            f2=next;
+        }
+        }
+        return arr;
+    }
+}
